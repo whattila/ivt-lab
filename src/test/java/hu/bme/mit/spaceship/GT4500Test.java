fef9f63e93fb7cdeb6fe9bb0b25747d7bc3fed9a
whattila <ahhidati070@gmail.com>
2021-05-14 14:14:01 +0200

    FireTorpedo_Single and All done

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -9,32 +9,54 @@ import static org.mockito.Mockito.*;
 public class GT4500Test {
 
   private GT4500 ship;
+  private TorpedoStore mockPrimary, mockSecondary;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    // Create mocks for the dependency TorpedoStore.
+    mockPrimary = mock(TorpedoStore.class);
+    mockSecondary = mock(TorpedoStore.class);
+
+    this.ship = new GT4500(mockPrimary, mockSecondary);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
+    // Set the behavior of the mocks: if they are called with
+    // parameter 1 then return the value true; if isEmpty is called,
+    // they return false.
+    when(mockPrimary.fire(1)).thenReturn(true);
+    when(mockSecondary.fire(1)).thenReturn(true);
+    when(mockPrimary.isEmpty()).thenReturn(false);
+    when(mockSecondary.isEmpty()).thenReturn(false);
 
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    // Verifying the mock: mockPrimary.fire(1) was called exactly once,
+    // while mockSecondary.fire(1) was not called,
+    // as it should be in GT4500's initial state and with the set mock behaviors.
+    verify(mockPrimary, times(1)).fire(1);
+    verify(mockSecondary, times(0)).fire(1);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
+    // Set the behavior of the mocks: if they are called with
+    // parameter 1 then the first returns the value false, the second returns the value true.
+    when(mockPrimary.fire(1)).thenReturn(false);
+    when(mockSecondary.fire(1)).thenReturn(true);
 
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    // Verifying the mock: both mockPrimary.fire(1) and mockSecondary.fire(1) was called exactly once
+    verify(mockPrimary, times(1)).fire(1);
+    verify(mockSecondary, times(1)).fire(1);
   }
 
 }
